7cecba2d374d79616e6a594643442ba9a71f788d
Krystian <krystian.izdebski1999@gmail.com>
2021-01-02 14:38:49 +0100

    Register api connection implementation

--- register.js
+++ register.js
@@ -1,8 +1,3 @@
-var user = {
-    login: "",
-    password: ""
-}
-
 function registerOperation(){
     var login = document.getElementById("RegisterLoginText").value;
     var password = document.getElementById("RegisterPasswordText").value;
@@ -12,14 +7,34 @@ function registerOperation(){
     document.getElementById("RegisterPasswordText").value = null;
     document.getElementById("RegisterPasswordRepeatText").value = null;
 
-    if(password == repeatPassword){
-        user.login = login;
-        user.password = password;
-        addUser(user);
-        document.getElementById("registerMessage").innerHTML = "Zarejestowano użytkownika pomyślnie";
+    var user = {
+        login: login,
+        password: password,
+        repeatedPassword: repeatPassword
     }
+
+    addUser(user);
 }
 
 function addUser(user){
+    var response;
     var xmr = new XMLHttpRequest();
+    xmr.open("POST", "");
+    xmr.responseType = "json";
+    xmr.setRequestHeader('Content-Type', 'application/json');
+
+    xmr.onload = function(){
+        response = xmr.response;
+
+        document.getElementById("registerMessage").innerHTML = response.message;
+        
+        var color;
+        if(response.type == true) color = "green";
+        else color = "red";
+
+        document.getElementById("registerMessage").style.backgroundColor = color;
+    }
+
+
+    xmr.send(JSON.stringify(user));
 }
